c1c15a1038efaf88222b61f2596d9863da45887e
saddab <saddabakhtar17@gmail.com>
2026-03-28 23:41:34 +0530

    feat: implement global exception handling for address service

---
+++ Address/src/main/java/com/sadcodes/address/exception/ErrorResponse.java
@@ -0,0 +1,23 @@
+package com.sadcodes.employee.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class ErrorResponse {
+
+    private String message;
+    private HttpStatus httpStatus;
+    private LocalDateTime localDateTime;
+
+    public ErrorResponse(String message,HttpStatus httpStatus){
+        this.message= message;
+        this.httpStatus= httpStatus;
+        this.localDateTime = LocalDateTime.now();
+    }
+
+}
